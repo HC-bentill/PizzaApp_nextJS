a919b32a89842f921604fcc5f30cff6cc499fb44
Henry <henrybentil88@gmail.com>
2022-04-05 11:15:41 +0000

    latest push

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -2,22 +2,33 @@ import dbconnect from "../../../util/mongo"
 import Product from "../../../models/Product"
 
 export default async function handler ( req, res ) {
-    const { method } = req;
+    const { method, query:{id}, } = req;
 
     dbconnect()
 
     //read/fetcg data from the db
     if ( method === "GET" ) {
         try {
-          const products = await Product.find()
-          res.status(200).json(products)  
+          const product = await Product.findById(id)
+          res.status(200).json(product)  
         } catch(err) {
             res.status(500).json(err)
         }
     }
 
     //send data to db from request body
-    if ( method === "POST" ) {
+    if ( method === "PUT" ) {
+        try {
+            //create records with the data from the request data with the productSchema
+            const product = await Product.create( req.body );
+            res.status(201).json(product)
+        } catch ( err ) {
+            //500 is server error
+            res.status(500).json(err)
+        }
+    }
+
+    if ( method === "DELETE" ) {
         try {
             //create records with the data from the request data with the productSchema
             const product = await Product.create( req.body );
--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({pizzaList}) {
 }
 
 export const getServerSideProps = async () => {
-  const res = axios.get( "http://localhost:3000/api/products" )
+  const res = await axios.get( "http://localhost:3000/api/products" )
   
   return {
     props: {
